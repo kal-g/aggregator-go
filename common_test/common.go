aa4c57c910c94fdf0db03a5ee628a58597ed1d7f
kal-g <kalana.gamlath1@gmail.com>
2020-08-26 10:43:47 -0700

    Add .circleci/config.yml (#2)

---
+++ common_test/common.go
@@ -0,0 +1,14 @@
+package common_test
+
+import (
+	"reflect"
+	"testing"
+)
+
+// AssertEqual checks if values are equal
+func AssertEqual(t *testing.T, a interface{}, b interface{}) {
+	if reflect.DeepEqual(a, b) {
+		return
+	}
+	t.Errorf("Received %v (type %v), expected %v (type %v)", a, reflect.TypeOf(a), b, reflect.TypeOf(b))
+}
