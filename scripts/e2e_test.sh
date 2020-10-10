f64e1bb5eaee100bd80be789c8dcc0e425470c15
Kal <kalana.gamlath1@gmail.com>
2020-10-09 21:31:06 -0700

    Fix namespace distribution

--- scripts/e2e_test.sh
+++ scripts/e2e_test.sh
@@ -22,14 +22,14 @@ done
 
 namespaceCount=`curl -s --header "Content-Type: application/json" --request POST --data '{"namespace":"test","metricKey":2,"metricID":1}' http://localhost:50051/count`
 echo "Namespace count was" $namespaceCount
-if [ $namespaceCount != "{\"ErrCode\":0,\"Count\":10000}" ]
+if [ $namespaceCount != "{\"Err\":null,\"Count\":10000}" ]
 then
   end
 fi
 
 namespaceInfo=`curl -s --header "Content-Type: application/json" --request POST --data '{"namespace":"test"}' http://localhost:50051/namespace/get_info`
 echo "Namespace info was" $namespaceInfo
-if [ $namespaceInfo != '{"error_code":0,"data":{"metric_keys":{"1":1}}}' ]
+if [ $namespaceInfo != '{"Err":null,"data":{"metric_keys":{"1":1}}}' ]
 then
   end
 fi
