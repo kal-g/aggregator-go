77947acf51565323a32df45e8fec20c897b3f86d
kal-g <kalana.gamlath1@gmail.com>
2020-11-20 17:35:06 -0800

    Make configmutator config public

--- pkg/configmutator/output.go
+++ pkg/configmutator/output.go
@@ -3,16 +3,16 @@ package configmutator
 func (cm ConfigMutator) GetConfig() map[string]interface{} {
 	events := []EventConfig{}
 	metrics := []MetricConfig{}
-	for _, e := range cm.c.Events {
+	for _, e := range cm.C.Events {
 		events = append(events, e)
 	}
-	for _, m := range cm.c.Metrics {
+	for _, m := range cm.C.Metrics {
 		metrics = append(metrics, m)
 	}
 	return map[string]interface{}{
-		"namespace":  cm.c.Namespace,
+		"namespace":  cm.C.Namespace,
 		"metrics":    metrics,
 		"events":     events,
-		"extra_info": cm.c.ExtraInfo,
+		"extra_info": cm.C.ExtraInfo,
 	}
 }
